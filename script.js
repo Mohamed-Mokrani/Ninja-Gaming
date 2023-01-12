25169204b6a88f7823013f89880a069225297f5a
Mohamed-Mokrani <med.mokranii@hotmail.fr>
2023-01-12 10:44:33 +0100

    update

--- script.js
+++ script.js
@@ -24,11 +24,16 @@ for (let i = 0; i < links.length; i++) {
 // games
 const imageDiv = document.getElementsByClassName("image-game");
 const game = document.getElementsByClassName("game");
+const options=document.getElementsByClassName("options");
 for (let i=0; i<game.length ; i++){
 game[i].addEventListener("mouseenter", function() {
   imageDiv[i].style.filter = "none";
-  imageDiv[i].style.width= "320px";
-  imageDiv[i].style.height= "190px";
+  imageDiv[i].style.width= "400px";
+  imageDiv[i].style.height= "240px";
+  game[i].style.height ="260px";
+  options[i].style.gap="30px";
+  game[i].style.fontSize="30px";
+  imageDiv[i].style.borderRadius="0px";
   
 });
 
@@ -36,5 +41,9 @@ game[i].addEventListener("mouseleave", function() {
   imageDiv[i].style.filter = "grayscale(100%)";
   imageDiv[i].style.width= "300px"
   imageDiv[i].style.height= "170px";
+  game[i].style.height ="200px";
+  options[i].style.gap="10px"
+  game[i].style.fontSize="20px";
+  imageDiv[i].style.borderRadius="10px";
 });
 }
